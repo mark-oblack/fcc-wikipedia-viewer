226c6cc67c4d8829339567b1071b6c47debcf626
mark-oblack <moblack42@gmail.com>
2018-06-27 07:50:29 -0400

    fix: changed scope of vars

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,13 @@
 //1b. A user clicks Random and are linked to a randome Wikipedia artilce in a new tab
 
 $(document).ready(function() {
-	var input;
-	var url;
-
 	$(".search-button").click(function() { //click function initiates when user clicks Search
-		input = document.getElementById("userSearch").value; //value in input field is stored in variable
+		let input = document.getElementById("userSearch").value; //value in input field is stored in variable
 		if(input==="" || input === " ") {
 			alert("Please enter search criteria.");
 		} else {
 			//value is placed in URL
-			url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&format=json&callback=?";
+			let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&format=json&callback=?";
 			//ajax call to get json data
 			$.ajax({
 				type: "GET",
